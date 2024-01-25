3f2aebf15a5528ba219965dd05f9b60ea1320f02
Gavin Clark <gavin.clark@hokodo.co>
2024-01-25 18:40:49 +0000

    Initial setup

---
+++ __tests__/hello.test.tsx
@@ -0,0 +1,9 @@
+import Home from "../app/page";
+
+import { fireEvent, render, screen } from "@testing-library/react";
+
+describe("Hello world", () => {
+  it("does this", async () => {
+    const x = render(<Home />);
+  });
+});
